5be75fe0e9790fc49f824d14ce2ed58578089784
James Baldwin <jamescbaldwin@gmail.com>
2020-10-10 13:10:15 -0400

    Kelvin to Fahrenheit, footer, etc.

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-//  var alert = alert("HAS script.js linked????")
-
-// $(document).ready(function() {
-
-    $("#search-button").click(function() {
+    //click funciton for search button (call search button by its "id")
+    $("#search-button").on("click", function() {
+        //event "preventDefault" prevents data from flashing
+        event.preventDefault();
         
-        
-        var apiKey = "91aa2321b032e187ef5be60685d90a73"
-        console.log('apiKey:', apiKey)
-        var city = $("#city").val();
-        var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "units=imperial&appid=91aa2321b032e187ef5be60685d90a73"
-            console.log(queryUrl);
-
-        if (city != ''){
+        //create variables to build url - 
+        //1 variable gets value of what is typed in search bar
+        var city = $("#citySearch").val();
+        //2nd variable builds complete url with base url + city variable + apiKey
+        var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=91aa2321b032e187ef5be60685d90a73";
+        //ajax function to retrieve a data response, given what was searched
             $.ajax({
-                queryUrl: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "units=imperial&appid=91aa2321b032e187ef5be60685d90a73",
+                url: queryUrl,
                 method: 'GET',
             }).then(function(response) {
-                event.preventDefault();
+                //log response of city searched
                 console.log(response);
-            })
+                var cityName = response.name;
+                var cityTemp = response.main.temp;
+                var cityFahrenheit = (cityTemp * 1.8) - 459.67
+                var cityHumidity = response.main.humidity;
+                var cityWind = response.wind.speed;
 
-        }else{
-            $("#error").html("Must input city name");
-        }
+                $("#cityName").html('<h4>' + cityName + '' + moment().format('L'));
+                $("#cityTemp").html('<h4>' + "Temperature(F): " + cityFahrenheit);
+                $("#cityHumidity").html('<h4>' + "Humidity: " + cityHumidity);
+                $("#cityWind").html('<h4>' + "Wind Speed: " + cityWind + "MPH");
+            });
 
-
-    });
-     function display(data) {
-        return
-     }
-// });
+        });
